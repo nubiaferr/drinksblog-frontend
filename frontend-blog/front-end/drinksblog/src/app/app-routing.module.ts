063cf7b184ea752a7029f22b5c965e2538f23fa4
nubiaferr <n.ferr@hotmail.com>
2021-08-25 11:55:47 -0300

    rotas

--- frontend-blog/front-end/drinksblog/src/app/app-routing.module.ts
+++ frontend-blog/front-end/drinksblog/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path: 'login', component: LoginComponent},
+  {path: 'signin', component: SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
