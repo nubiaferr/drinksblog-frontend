7030674bc107c6136bf42cc72865f06f01a82435
nubiaferr <n.ferr@hotmail.com>
2021-08-27 14:20:00 -0300

    models

---
+++ frontend-blog/front-end/drinksblog/src/app/model/User.ts
@@ -0,0 +1,10 @@
+import { Post } from './Post';
+export class User{
+  public id: number
+  public name: string
+  public password: string
+  public photo: string
+  public username: string
+  public userType: string
+  public post: Post
+}
---
+++ frontend-blog/front-end/drinksblog/src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+  public id: number
+  public name: string
+  public password: string
+  public photo: string
+  public username: string
+  public userType: string
+  public token: string
+}
