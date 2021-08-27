b424c32478b853e4248ed8d68f44242121e985f4
nubiaferr <n.ferr@hotmail.com>
2021-08-27 20:39:40 -0300

    Auth login/signin

--- frontend-blog/front-end/drinksblog/src/app/app.module.ts
+++ frontend-blog/front-end/drinksblog/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
@@ -18,7 +19,8 @@ import { SigninComponent } from './signin/signin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-blog/front-end/drinksblog/src/app/service/auth.service.ts
+++ frontend-blog/front-end/drinksblog/src/app/service/auth.service.ts
@@ -1,9 +1,23 @@
+import { UserLogin } from './../model/UserLogin';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+    ) { }
+
+  login(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>('https://nubiaferrdrinks.herokuapp.com/users/login', userLogin)
+  }
+
+  signin(user: User): Observable<User>{
+    return this.http.post<User>('https://nubiaferrdrinks.herokuapp.com/users/signin', user)
+  }
 }
