a32c1e9d1645ece68159b8cccdd75535ba967d16
nubiaferr <n.ferr@hotmail.com>
2021-08-27 19:25:12 -0300

    Consume

--- frontend-blog/front-end/drinksblog/src/app/app.module.ts
+++ frontend-blog/front-end/drinksblog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
