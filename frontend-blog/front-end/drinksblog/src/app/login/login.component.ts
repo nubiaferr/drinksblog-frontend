cad61214d4b98b2279f52e620df8baaa3d5f0f3c
nubiaferr <n.ferr@hotmail.com>
2021-08-28 17:35:17 -0300

    login component consume

--- frontend-blog/front-end/drinksblog/src/app/login/login.component.ts
+++ frontend-blog/front-end/drinksblog/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../environments/environment.prod';
 import { AuthService } from './../service/auth.service';
 import { UserLogin } from './../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  userLogin: UserLogin = new UserLogin
+  userLogin: UserLogin = new UserLogin()
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,7 +24,23 @@ export class LoginComponent implements OnInit {
   login(){
     this.auth.login(this.userLogin).subscribe((resp: UserLogin)=>{
       this.userLogin = resp
-      
+
+      //setando as infos
+      environment.token = this.userLogin.token
+      environment.name = this.userLogin.name
+      environment.photo = this.userLogin.photo
+      environment.id = this.userLogin.id
+
+      /*console.log(environment.token)
+      console.log(environment.name)
+      console.log(environment.photo)
+      console.log(environment.id)*/
+
+      this.router.navigate(['/homepage'])
+    }, error => {
+      if(error.status == 500){
+        alert('Username/password: incorrect')
+      }
     })
   }
 
